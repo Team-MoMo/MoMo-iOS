2fe8b9877fd34f96b2e5f592f3f22acc3bd30467
iamcho2 <thatgrass@naver.com>
2020-12-28 20:25:59 +0900

    [#4] AppConstants 기초 설정

---
+++ MoMo/MoMo/Resources/Constants/AppConstants.swift
@@ -0,0 +1,41 @@
+//
+//  AppConstants.swift
+//  MoMo
+//
+//  Created by 초이 on 2020/12/28.
+//
+
+import Foundation
+import UIKit
+
+struct Constants {
+    
+    // MARK: - Design Constants
+    
+    struct Design {
+        
+        struct Color {
+            /// Asset으로 Color 관리 할 거라 필요할 진 모르겠지만..
+            /// 예시 : static let Blue = UIColor.rgba(red: 0, green: 122, blue: 255, alpha: 1) (Extension 함수 추가 필요)
+            /// 사용 : Constants.Design.Color.Blue
+            
+        }
+        
+        struct Image {
+            /// 예시 : static let IconHome = UIImage(named: "ico_category")
+        }
+        
+        struct Font {
+            /// 예시 : static let Body = UIFont.systemFont(ofSize: 16, weight: .regular)
+            /// 사용 : label.font = Constants.Design.Font.Body
+        }
+        
+    }
+    
+    // MARK: - Content Constants
+    
+    struct Content {
+        /// 예시 : static let Category = "category"
+    }
+    
+}
