ee2af26a265f8d41429f04090ec7e6a91a08ffc5
iamcho2 <thatgrass@naver.com>
2021-01-02 00:07:50 +0900

    [#7] Color assets 추가

---
+++ MoMo/MoMo/Sources/Extensions/UIColor+Extension.swift
@@ -0,0 +1,34 @@
+//
+//  UIColor+Extension.swift
+//  MoMo
+//
+//  Created by 초이 on 2021/01/01.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let Black1: UIColor = UIColor(named: "Black1")!
+    static let Black2Nav: UIColor = UIColor(named: "Black2Nav")!
+    static let Black3List: UIColor = UIColor(named: "Black3List")!
+    static let Black4: UIColor = UIColor(named: "Black4")!
+    static let Black5Publish: UIColor = UIColor(named: "Black5Publish")!
+    static let Black6: UIColor = UIColor(named: "Black6")!
+    static let Blue1: UIColor = UIColor(named: "Blue1")!
+    static let Blue2: UIColor = UIColor(named: "Blue2")!
+    static let Blue3: UIColor = UIColor(named: "Blue3")!
+    static let Blue4: UIColor = UIColor(named: "Blue4")!
+    static let Blue5: UIColor = UIColor(named: "Blue5")!
+    static let Blue6: UIColor = UIColor(named: "Blue6")!
+    static let Blue7: UIColor = UIColor(named: "Blue7")!
+    static let BlueModalAble: UIColor = UIColor(named: "BlueModalAble")!
+    static let LineFilterGray: UIColor = UIColor(named: "LineFilterGray")!
+    static let LineLightGray: UIColor = UIColor(named: "LineLightGray")!
+    static let ListBlack: UIColor = UIColor(named: "ListBlack")!
+    static let ListEmotion: UIColor = UIColor(named: "ListEmotion")!
+    static let ListLightBlack: UIColor = UIColor(named: "ListLightBlack")!
+    static let ListSubGray: UIColor = UIColor(named: "ListSubGray")!
+    static let NavWhite: UIColor = UIColor(named: "NavWhite")!
+    static let TitleNavy: UIColor = UIColor(named: "TitleNavy")!
+}
