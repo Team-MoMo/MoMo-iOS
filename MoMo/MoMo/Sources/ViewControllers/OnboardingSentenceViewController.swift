d64d64452cf7b3f220e9e19b9b0a1ca72ab380c3
haeseoklee <haeseoklee.dev@gmail.com>
2021-01-09 00:22:12 +0900

    OnboardingSentenceViewController 구현

--- MoMo/MoMo/Sources/ViewControllers/OnboardingSentenceViewController.swift
+++ MoMo/MoMo/Sources/ViewControllers/OnboardingSentenceViewController.swift
@@ -9,21 +9,54 @@ import UIKit
 
 class OnboardingSentenceViewController: UIViewController {
 
+    @IBOutlet weak var firstButton: UIButton!
+    @IBOutlet weak var secondButton: UIButton!
+    @IBOutlet weak var thirdButton: UIButton!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var moodLabel: UILabel!
+    @IBOutlet weak var moodIcon: UIImageView!
+    
+    @IBOutlet weak var firstAuthorLabel: UILabel!
+    @IBOutlet weak var firstBookTitleLabel: UILabel!
+    @IBOutlet weak var firstPublisherLabel: UILabel!
+    @IBOutlet weak var firstSentenceLabel: UILabel!
+            
+    @IBOutlet weak var secondAuthorLabel: UILabel!
+    @IBOutlet weak var secondBookTitleLabel: UILabel!
+    @IBOutlet weak var secondPublisherLabel: UILabel!
+    @IBOutlet weak var secondSentenceLabel: UILabel!
+    
+    @IBOutlet weak var thirdAuthorLabel: UILabel!
+    @IBOutlet weak var thirdBookTitleLabel: UILabel!
+    @IBOutlet weak var thirdPublisherLabel: UILabel!
+    @IBOutlet weak var thirdSentenceLabel: UILabel!
+    
+    private var buttons: [Button] = []
+    var mood: Mood?
+    var date: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.buttons = [Button(button: firstButton), Button(button: secondButton), Button(button: thirdButton)]
+        
+        for button in buttons {
+            button.buttonsRoundedUp()
+            button.buttonsAddShadow()
+        }
+        
+        self.moodLabel.text = self.mood?.toString()
+        self.moodIcon.image = self.mood?.toIcon()
+        self.dateLabel.text = self.date
+        
+    }
 
-        // Do any additional setup after loading the view.
+    @IBAction func firstButtonTouchUp(_ sender: UIButton) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func secondButtonTouchUp(_ sender: UIButton) {
     }
-    */
-
+    @IBAction func thirdButtonTouchUp(_ sender: UIButton) {
+    }
 }
