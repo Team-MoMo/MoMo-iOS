2aeec2173309d79a4c478967c5b41e9e2c04567f
haeseoklee <haeseoklee.dev@gmail.com>
2021-01-09 00:21:06 +0900

    vc 인스턴스에 형변환 코드 추가

--- MoMo/MoMo/Sources/ViewControllers/OnboardingViewController.swift
+++ MoMo/MoMo/Sources/ViewControllers/OnboardingViewController.swift
@@ -9,7 +9,6 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
 
-
     @IBOutlet weak var tutorialStartButton: UIButton!
 
     override func viewDidLoad() {
@@ -20,19 +19,9 @@ class OnboardingViewController: UIViewController {
     
     @IBAction func tutorialButtonTouchUp(_ sender: UIButton) {
         
-        guard let onboardingMoodViewController = self.storyboard?.instantiateViewController(identifier: "OnboardingMoodViewController") else { return }
+        guard let onboardingMoodViewController = self.storyboard?.instantiateViewController(identifier: Constants.Identifier.onboardingMoodViewController) as? OnboardingMoodViewController else { return }
         
         self.navigationController?.pushViewController(onboardingMoodViewController, animated: true)
         
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
