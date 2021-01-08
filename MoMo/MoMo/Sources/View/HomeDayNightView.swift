3ef3eccd9b92c9fdd0ff6a750edb55b3c18bfd48
iamcho2 <thatgrass@naver.com>
2021-01-09 00:06:29 +0900

    [#29] 자간, 행간 지정

--- MoMo/MoMo/Sources/View/HomeDayNightView.swift
+++ MoMo/MoMo/Sources/View/HomeDayNightView.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class HomeDayNightView: UIView {
+    
+    // TODO: - noDiaryStackView, filledDiaryView y축 autolayout은 나중에 오브제 나오면 오브제 중 구름에 맞춰야 함
 
     // MARK: - @IBOutlet Properties
     
@@ -34,6 +36,7 @@ class HomeDayNightView: UIView {
     
     // MARK: - Functions
     
+    // gradient 생성 후 배경 지정
     func createGradientLayer() {
         gradientLayer = CAGradientLayer()
         gradientLayer.frame = self.backgroundView.bounds
@@ -41,13 +44,37 @@ class HomeDayNightView: UIView {
         self.backgroundView.layer.addSublayer(gradientLayer)
     }
     
+    func textSpacing(labelName: UILabel, lineSpacing: Int) {
+        let attributedString = NSMutableAttributedString(string: labelName.text ?? "")
+        let paragraphStyle = NSMutableParagraphStyle()
+        
+        paragraphStyle.lineSpacing = CGFloat(lineSpacing)
+        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSRange(location: 0, length: attributedString.length))
+        attributedString.addAttribute(NSAttributedString.Key.kern, value: -0.6, range: NSRange(location: 0, length: attributedString.length))
+
+        labelName.attributedText = attributedString
+    }
+    
     // MARK: - View Life Cycle
     
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        print("asd")
+        // gradient 배경 지정
         createGradientLayer()
+        
+        // TODO: - 줄 간격, 자간 extension으로 빼기 - 리팩토링 필요
+        // 자간은 앱 내 통일이나, 행간은 다름
+        
+        textSpacing(labelName: dateLabel, lineSpacing: 7)
+        textSpacing(labelName: emotionLabel, lineSpacing: 10)
+        textSpacing(labelName: depthLabel, lineSpacing: 10)
+        textSpacing(labelName: quoteLabel, lineSpacing: 10)
+        textSpacing(labelName: writerLabel, lineSpacing: 10)
+        textSpacing(labelName: bookTitleLabel, lineSpacing: 10)
+        textSpacing(labelName: publisherLabel, lineSpacing: 10)
+        textSpacing(labelName: diaryLabel, lineSpacing: 10)
+        
     }
 
 }
