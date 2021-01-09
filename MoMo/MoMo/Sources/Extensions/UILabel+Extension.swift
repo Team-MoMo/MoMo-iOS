bf4ae2d5a33a08a15e166a417b7bcefd20a3ce44
haeseoklee <haeseoklee.dev@gmail.com>
2021-01-10 01:41:19 +0900

    UILabel Extension 추가

--- MoMo/MoMo/Sources/Extensions/UILabel+Extension.swift
+++ MoMo/MoMo/Sources/Extensions/UILabel+Extension.swift
@@ -5,4 +5,22 @@
 //  Created by Haeseok Lee on 2021/01/09.
 //
 
-import Foundation
+import UIKit
+
+extension UILabel {
+    
+    func startBlink() {
+        UIView.animate(
+            withDuration: 0.3,
+            delay: 0.0,
+            options: [.allowUserInteraction, .curveEaseInOut, .autoreverse, .repeat],
+            animations: { self.alpha = 0 },
+            completion: nil
+        )
+    }
+    
+    func stopBlink() {
+        layer.removeAllAnimations()
+        alpha = 1
+    }
+}
