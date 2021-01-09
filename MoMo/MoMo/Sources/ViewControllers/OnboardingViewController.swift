04b953cac376643318c540a5a6baca189f66cbbb
haeseoklee <haeseoklee.dev@gmail.com>
2021-01-10 04:19:19 +0900

    OnboardingViewController 구현

--- MoMo/MoMo/Sources/ViewControllers/OnboardingViewController.swift
+++ MoMo/MoMo/Sources/ViewControllers/OnboardingViewController.swift
@@ -11,18 +11,58 @@ class OnboardingViewController: UIViewController {
 
     // MARK: - @IBOutlet Properties
     
-    @IBOutlet weak var tutorialStartButton: UIButton!
-
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    // MARK: - Life Cycles
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.navigationControllerSetUp()
+        self.buttonRoundedUp()
+        self.hideDesciptionLabelWithAnimation()
     }
     
-    @IBAction func tutorialButtonTouchUp(_ sender: UIButton) {
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         
+        self.showDesciptionLabelWithAnimation()
+    }
+    
+    // MARK: - Functions
+    
+    func navigationControllerSetUp() {
+        self.navigationController?.navigationBar.topItem?.title = ""
+        self.navigationController?.isNavigationBarHidden = true
+    }
+    
+    func buttonRoundedUp() {
+        self.startButton.layer.cornerRadius = self.startButton.frame.size.height / 2
+        self.startButton.clipsToBounds = true
+    }
+    
+    func hideDesciptionLabelWithAnimation() {
+        self.descriptionLabel.alpha = 0.0
+    }
+    
+    func showDesciptionLabelWithAnimation() {
+        UIView.animate(
+            withDuration: 1.0,
+            delay: 0,
+            animations: {
+                self.descriptionLabel.alpha = 1.0
+            }
+        )
+    }
+    
+    @IBAction func startButtonTouchUp(_ sender: UIButton) {
         guard let onboardingMoodViewController = self.storyboard?.instantiateViewController(identifier: Constants.Identifier.onboardingMoodViewController) as? OnboardingMoodViewController else { return }
         
         self.navigationController?.pushViewController(onboardingMoodViewController, animated: true)
-        
+    }
+    
+    @IBAction func loginButtonTouchUp(_ sender: UIButton) {
     }
 }
