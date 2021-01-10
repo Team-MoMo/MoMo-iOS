d3111889e59c13691bb73baa296fef59edff8ad7
iamcho2 <thatgrass@naver.com>
2021-01-10 18:33:14 +0900

    [#45] extension 함수 String에 맞게 재수정

--- MoMo/MoMo/Sources/Extensions/String+Extension.swift
+++ MoMo/MoMo/Sources/Extensions/String+Extension.swift
@@ -18,14 +18,14 @@ extension String {
     
     // 자간, 행간 지정
     // 앱 내 모든 자간 -0.6으로 통일, 행간은 Int값으로 지정 가능
-    func textSpacing(lineSpacing: Int) {
-        let attributedString = NSMutableAttributedString(string: labelName.text ?? "")
+    func textSpacing(lineSpacing: Int) -> NSMutableAttributedString {
+        let attributedString = NSMutableAttributedString(string: self)
         let paragraphStyle = NSMutableParagraphStyle()
         
         paragraphStyle.lineSpacing = CGFloat(lineSpacing)
         attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraphStyle, range: NSRange(location: 0, length: attributedString.length))
         attributedString.addAttribute(NSAttributedString.Key.kern, value: -0.6, range: NSRange(location: 0, length: attributedString.length))
 
-        labelName.attributedText = attributedString
+        return attributedString
     }
 }
