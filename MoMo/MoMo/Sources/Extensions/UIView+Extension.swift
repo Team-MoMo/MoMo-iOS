90a184d2bbe2bae13622ecbd9ca8116391664233
haeseoklee <haeseoklee.dev@gmail.com>
2021-01-11 22:47:59 +0900

    [#14] UIColor, UIView extension에 코드 추가 UISlider UIImage extenstion 추가

--- MoMo/MoMo/Sources/Extensions/UIView+Extension.swift
+++ MoMo/MoMo/Sources/Extensions/UIView+Extension.swift
@@ -17,4 +17,18 @@ extension UIView {
         shapeLayer.path = bezierPath.cgPath
         self.layer.mask = shapeLayer
     }
+    
+    // Nib 생성
+    static func initFromNib<T: UIView>() -> T? {
+        return Bundle.main.loadNibNamed(String(describing: self), owner: nil, options: nil)?[0] as? T
+    }
+    
+    // UIView를 UIImage로 변환
+    func asImage() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+        return renderer.image { rendererContext in
+            layer.render(in: rendererContext.cgContext)
+        }
+    }
+
 }
