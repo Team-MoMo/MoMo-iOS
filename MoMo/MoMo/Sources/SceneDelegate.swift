dbd1c24abd57a8c2ca68f463478ebece903f9913
haeseoklee <haeseoklee.dev@gmail.com>
2021-01-14 00:48:26 +0900

    [#97] SceneDelegate 에 주석 줄바꿈 수정

--- MoMo/MoMo/Sources/SceneDelegate.swift
+++ MoMo/MoMo/Sources/SceneDelegate.swift
@@ -15,8 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-//        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-//         This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        //This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         
         let splashStoryboard = UIStoryboard(name: "Splash", bundle: nil)
         let splashViewController = splashStoryboard.instantiateViewController(withIdentifier: "SplashViewController")
