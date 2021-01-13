5df952032f858af8943210db947050ce81615adc
iamcho2 <thatgrass@naver.com>
2021-01-14 03:48:41 +0900

    [#109] SignUpData Model

---
+++ MoMo/MoMo/Sources/Models/SignUpData.swift
@@ -0,0 +1,26 @@
+//
+//  SignUpData.swift
+//  MoMo
+//
+//  Created by 초이 on 2021/01/14.
+//
+
+import Foundation
+
+// MARK: - SignUpData
+
+struct SignUpData: Codable {
+    let user: User
+    let token: String
+}
+
+// MARK: - User
+
+struct User: Codable {
+    let id: Int
+    let email, password, passwordSalt: String
+    let isAlarmSet: Bool
+    let alarmTime, tempPassword, tempPasswordCreatedAt: String
+    let tempPasswordIssueCount: Int
+    let createdAt, updatedAt: String
+}
