70d1213f633172dece89635ad1d5f7d2b3f22e8e
Haeseok Lee <haeseoklee.dev@gmail.com>
2021-01-13 19:32:36 +0900

    Revert "[#9] List filter modal"

--- MoMo/MoMo/Sources/SceneDelegate.swift
+++ MoMo/MoMo/Sources/SceneDelegate.swift
@@ -18,7 +18,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 //        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
 //         This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         
-
         let splashStoryboard = UIStoryboard(name: "Splash", bundle: nil)
         let splashViewController = splashStoryboard.instantiateViewController(withIdentifier: "SplashViewController")
         self.window?.rootViewController = splashViewController
