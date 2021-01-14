0ade185b4a02ef52c53b262cf1d60a215b8561f0
haeseoklee <haeseoklee.dev@gmail.com>
2021-01-15 07:38:31 +0900

    [#152] [#153] DiaryWithId 모델 수정

--- MoMo/MoMo/Sources/Models/DiaryWithId.swift
+++ MoMo/MoMo/Sources/Models/DiaryWithId.swift
@@ -1,8 +1,25 @@
-//
-//  DiaryWithID.swift
-//  MoMo
-//
-//  Created by Haeseok Lee on 2021/01/15.
-//
+// MARK: - DataClass
+struct DiaryWithId: Codable {
+    let id, position, depth: Int
+    let contents, wroteAt: String
+    let userID, sentenceID, emotionID: Int
+    let createdAt, updatedAt: String
+    let sentence: Sentence
+    let emotion: EmotionDiaryWithId
 
-import Foundation
+    enum CodingKeys: String, CodingKey {
+        case id, position, depth, contents, wroteAt
+        case userID = "userId"
+        case sentenceID = "sentenceId"
+        case emotionID = "emotionId"
+        case createdAt, updatedAt
+        case sentence = "Sentence"
+        case emotion = "EmotionDiaryWithId"
+    }
+}
+
+// MARK: - Emotion
+struct EmotionDiaryWithId: Codable {
+    let id: Int
+    let name, createdAt, updatedAt: String
+}
