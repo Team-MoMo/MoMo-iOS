37c416c6672c16dd20de5592229357675fa1e462
haeseoklee <haeseoklee.dev@gmail.com>
2021-01-29 03:00:56 +0900

    [#216] AppDiary 속성변경

--- MoMo/MoMo/Sources/AppModels/AppDiary.swift
+++ MoMo/MoMo/Sources/AppModels/AppDiary.swift
@@ -9,10 +9,7 @@ import UIKit
 
 // TODO: - 해석이오빠가 리팩토링하면서 네이밍 변경하거나 없애기
 struct AppDiary {
-    var date: String
-    var year: Int
-    var month: Int
-    var day: Int
+    var date: AppDate
     var mood: AppEmotion
     var depth: AppDepth
     var sentence: AppSentence
