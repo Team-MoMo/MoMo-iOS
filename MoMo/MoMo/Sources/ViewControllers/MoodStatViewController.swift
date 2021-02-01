a77a8ed027473facf0b19b28106d18930bf1b206
jeongyeob97 <leejeongyeob97@gmail.com>
2021-02-02 03:14:44 +0900

    [#227] 감정 통계뷰 구현

--- MoMo/MoMo/Sources/ViewControllers/MoodStatViewController.swift
+++ MoMo/MoMo/Sources/ViewControllers/MoodStatViewController.swift
@@ -8,22 +8,53 @@
 import UIKit
 
 class MoodStatViewController: UIViewController {
-
+    
+    // MARK: - Constants
+    let multiplier: Float = 0.35
+    
+    // MARK: - Properties
+    var maximum: CGFloat?
+    var statViewMaximumHeight: CGFloat?
+    var moodStatData: [Int] = [6,1,8,3,5,4,3,2]
+    
+    
+    // MARK: -IBOutlets
+    @IBOutlet var statNumLabel: Array<UILabel>?
+    @IBOutlet var statViewHeight: Array<NSLayoutConstraint>?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        initializeMaximum()
+        intializemStatViewMaximumHeight()
+        intializeHeightConstraints()
+        initializeLabels()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func initializeMaximum() {
+        guard let max = moodStatData.max() else {
+            return
+        }
+        maximum = CGFloat(max)
     }
-    */
-
+    private func intializemStatViewMaximumHeight() {
+        statViewMaximumHeight = self.view.frame.height * CGFloat(multiplier)
+    }
+    
+    private func intializeHeightConstraints() {
+        guard let statHeights = statViewHeight, let viewHeight = statViewMaximumHeight, let max = maximum else {
+            return
+        }
+        for constraint in 0..<statHeights.count {
+            statHeights[constraint].constant = viewHeight * (CGFloat(moodStatData[constraint])/max)
+        }
+    }
+    
+    private func initializeLabels() {
+        guard let labels = statNumLabel else {
+            return
+        }
+        for num in 0..<labels.count {
+            labels[num].text = "\(moodStatData[num])"
+        }
+    }
 }
