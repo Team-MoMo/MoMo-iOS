738d780ade54ec23e417f16047927b09fb523c8c
haeseoklee <haeseoklee.dev@gmail.com>
2021-02-01 22:38:32 +0900

    [#216] DiaryViewConroller 리팩토링

--- MoMo/MoMo/Sources/ViewControllers/DiaryViewController.swift
+++ MoMo/MoMo/Sources/ViewControllers/DiaryViewController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+enum DiaryViewNavigationButton: Int {
+    case leftButton = 0, rightButton
+}
+
 class DiaryViewController: UIViewController {
     
     @IBOutlet weak var fish1: UIImageView!
@@ -44,14 +48,14 @@ class DiaryViewController: UIViewController {
     
     lazy var leftButton: UIBarButtonItem = {
         let button = UIBarButtonItem(image: Constants.Design.Image.btnBackWhite, style: .plain, target: self, action: #selector(buttonPressed(sender:)))
-        button.tag = NavigationButton.leftButton.rawValue
+        button.tag = DiaryViewNavigationButton.leftButton.rawValue
         button.tintColor = UIColor.white
         return button
     }()
     
     lazy var rightButton: UIBarButtonItem = {
         let button = UIBarButtonItem(image: UIImage(named: "icSubtab"), style: .done, target: self, action: #selector(buttonPressed(sender:)))
-        button.tag = NavigationButton.rightButton.rawValue
+        button.tag = DiaryViewNavigationButton.rightButton.rawValue
         button.tintColor = UIColor.white
         return button
     }()
@@ -211,9 +215,9 @@ class DiaryViewController: UIViewController {
     @objc private func buttonPressed(sender: Any) {
         if let button = sender as? UIBarButtonItem {
             switch button.tag {
-            case NavigationButton.leftButton.rawValue:
+            case DiaryViewNavigationButton.leftButton.rawValue:
                 self.popToHomeViewController()
-            case NavigationButton.rightButton.rawValue:
+            case DiaryViewNavigationButton.rightButton.rawValue:
                 if self.menuToggleFlag {
                     self.menuView?.removeFromSuperview()
                 } else {
@@ -340,7 +344,7 @@ extension DiaryViewController: UploadModalViewDelegate {
 // MARK: - DiaryWriteViewControllerDelegate
 
 extension DiaryViewController: DiaryWriteViewControllerDelegate {
-    func popDiaryWirteViewController(diaryInfo: AppDiary?) {
+    func popToDiaryViewController(diaryInfo: AppDiary?) {
         self.menuView?.removeFromSuperview()
         
         guard let safeDiaryInfo = self.diaryInfo else { return }
