d112382336cef59a918b55cae332d6b96959547f
haeseoklee <haeseoklee.dev@gmail.com>
2021-02-11 02:19:12 +0900

    [#252] SettingVC에 회원탈퇴 버튼 추가 및 API 연결

--- MoMo/MoMo/Sources/ViewControllers/SettingViewController.swift
+++ MoMo/MoMo/Sources/ViewControllers/SettingViewController.swift
@@ -13,6 +13,10 @@ enum SettingViewUsage: Int {
     case setting = 0, info
 }
 
+enum SettingAlertViewUsage: Int {
+    case logout = 0, withdrawal
+}
+
 class SettingViewController: UIViewController {
     
     // MARK: - IBOutlets
@@ -23,6 +27,7 @@ class SettingViewController: UIViewController {
     
     var settingViewUsage: SettingViewUsage?
     var lockIsUpdated: Bool = false
+    private var settingAlerViewUsage: SettingAlertViewUsage?
     private var toastView: ToastView?
     private let cellHeight: CGFloat = 64
     private var cellInfos: [SettingCellInfo]?
@@ -44,6 +49,16 @@ class SettingViewController: UIViewController {
         let button: UIButton = UIButton()
         button.setImage(Constants.Design.Image.btnResetting, for: .normal)
         button.addTarget(self, action: #selector(touchResetButton(sender:)), for: .touchUpInside)
+        return button
+    }()
+    private lazy var withdrawalButton: UIButton = {
+        let button: UIButton = UIButton()
+        let buttonLabelText: String = "회원탈퇴"
+        let buttonLabelColor = UIColor.Black6
+        let attributes: [NSAttributedString.Key: Any] = [NSAttributedString.Key.font: UIFont(name: "AppleSDGothicNeo-Regular", size: 14)!, NSAttributedString.Key.foregroundColor: buttonLabelColor]
+        let buttonLabelAttributedText = NSAttributedString(string: buttonLabelText, attributes: attributes)
+        button.setAttributedTitle(buttonLabelAttributedText, for: .normal)
+        button.addTarget(self, action: #selector(touchWithDrawalButton(sender:)), for: .touchUpInside)
         return button
     }()
     private lazy var versionLabel: UILabel = {
@@ -158,7 +173,11 @@ class SettingViewController: UIViewController {
             (image: Constants.Design.Image.icPwChange, labelText: "비밀번호 변경", touchAction: self.pushToPasswordChangeViewController),
             (image: Constants.Design.Image.icDoc1, labelText: "개인정보처리방침", touchAction: self.pushToPersonalTermViewController),
             (image: Constants.Design.Image.icDoc2, labelText: "서비스이용약관", touchAction: self.pushToServiceTermViewController),
-            (image: Constants.Design.Image.icLogout, labelText: "로그아웃", touchAction: self.attachAlertModalView)
+            (image: Constants.Design.Image.icLogout, labelText: "로그아웃", touchAction: {
+                self.settingAlerViewUsage = .logout
+                self.attachAlertModalView(alertLabelText: "정말 로그아웃 하시겠어요?\n일기를 다시 쓰려면 로그인해 주세요!")
+            }),
+            (image: UIImage(), labelText: "", touchAction: {})
         ]
     }
     
@@ -172,8 +191,8 @@ class SettingViewController: UIViewController {
         self.updateResetButtonConstraints(superView: superView)
     }
     
-    private func attachAlertModalView() {
-        self.alertModalView = AlertModalView.instantiate(alertLabelText: "정말 로그아웃 하시겠어요?\n일기를 다시 쓰려면 로그인해 주세요!", leftButtonTitle: "확인", rightButtonTitle: "취소")
+    private func attachAlertModalView(alertLabelText: String) {
+        self.alertModalView = AlertModalView.instantiate(alertLabelText: alertLabelText, leftButtonTitle: "확인", rightButtonTitle: "취소")
         if let alertModalView = self.alertModalView {
             alertModalView.alertModalDelegate = self
             self.view.insertSubview(alertModalView, aboveSubview: self.view)
@@ -183,6 +202,46 @@ class SettingViewController: UIViewController {
     
     private func attachVersionLabel() {
         self.settingTableView.addSubview(self.versionLabel)
+    }
+    
+    private func attachToastView(message: String) {
+        self.toastView = ToastView.instantiate(message: message)
+        guard let toastView = self.toastView else { return }
+        toastView.alpha = 0.0
+        self.view.insertSubview(toastView, aboveSubview: self.view)
+        self.updateToastViewConstraints(view: toastView)
+    }
+    
+    private func attachWithdrawalButton(superView: UITableViewCell) {
+        superView.addSubview(self.withdrawalButton)
+        self.updateWithdrawalButtonConstraints(superView: superView)
+    }
+    
+    private func attachToastViewWithAnimation(message: String) {
+        self.attachToastView(message: message)
+        UIView.animate(
+            withDuration: 0.5,
+            delay: 0,
+            animations: {
+                self.toastView?.alpha = 1.0
+            },
+            completion: { _ in
+                UIView.animate(
+                    withDuration: 0.5,
+                    delay: 0.5,
+                    animations: {
+                        self.toastView?.alpha = 0.0
+                    },
+                    completion: { _ in
+                        self.detachToastView()
+                    }
+                )
+            }
+        )
+    }
+    
+    private func detachToastView() {
+        self.toastView?.removeFromSuperview()
     }
     
     private func showResetButton() {
@@ -225,7 +284,20 @@ class SettingViewController: UIViewController {
         }
     }
     
+    private func updateWithdrawalButtonConstraints(superView: UITableViewCell) {
+        self.withdrawalButton.snp.makeConstraints { make in
+            make.centerY.equalTo(superView.snp.centerY)
+            make.trailing.equalTo(superView.snp.trailing).inset(7)
+        }
+    }
+    
     private func updateAlertModalViewConstraints(view: UIView) {
+        view.snp.makeConstraints({ (make) in
+            make.width.height.centerX.centerY.equalTo(self.view)
+        })
+    }
+    
+    private func updateToastViewConstraints(view: UIView) {
         view.snp.makeConstraints({ (make) in
             make.width.height.centerX.centerY.equalTo(self.view)
         })
@@ -241,50 +313,6 @@ class SettingViewController: UIViewController {
         guard let infoViewController = settingStoryboard.instantiateViewController(identifier: Constants.Identifier.settingViewController) as? SettingViewController else { return }
         infoViewController.settingViewUsage = .info
         self.navigationController?.pushViewController(infoViewController, animated: true)
-    }
-    
-    private func attachToastViewWithAnimation(message: String) {
-        self.attachToastView(message: message)
-        UIView.animate(
-            withDuration: 0.5,
-            delay: 0,
-            animations: {
-                self.toastView?.alpha = 1.0
-            },
-            completion: { _ in
-                UIView.animate(
-                    withDuration: 0.5,
-                    delay: 0.5,
-                    animations: {
-                        self.toastView?.alpha = 0.0
-                    },
-                    completion: { _ in
-                        self.detachToastView()
-                    }
-                )
-            }
-        )
-    }
-    
-    private func attachToastView(message: String) {
-        self.toastView = ToastView.instantiate(message: message)
-        guard let toastView = self.toastView else { return }
-        toastView.alpha = 0.0
-        self.view.insertSubview(toastView, aboveSubview: self.view)
-        self.updateToastViewConstraints(view: toastView)
-    }
-    
-    private func updateToastViewConstraints(view: UIView) {
-        view.snp.makeConstraints({ (make) in
-            make.width.equalTo(self.view)
-            make.height.equalTo(self.view)
-            make.centerX.equalTo(self.view)
-            make.centerY.equalTo(self.view)
-        })
-    }
-    
-    private func detachToastView() {
-        self.toastView?.removeFromSuperview()
     }
     
     private func pushToLockViewController() {
@@ -380,13 +408,26 @@ class SettingViewController: UIViewController {
     @objc func touchResetButton(sender: UIButton) {
         self.pushToLockViewController()
     }
+    
+    @objc func touchWithDrawalButton(sender: UIButton) {
+        self.settingAlerViewUsage = .withdrawal
+        self.attachAlertModalView(alertLabelText: "정말 탈퇴 하시겠어요?\n작성된 일기를 다시 볼 수 없어요!")
+    }
 }
 
 // MARK: - AlertModalViewDelegate
 
 extension SettingViewController: AlertModalDelegate {
     func leftButtonTouchUp(button: UIButton) {
-        self.popToLoginViewController()
+        switch self.settingAlerViewUsage {
+        case .logout:
+            self.popToLoginViewController()
+        case .withdrawal:
+            self.deleteUserWithAPI(userId: APIConstants.userId, completion: self.popToLoginViewController)
+        default:
+            return
+        }
+        
     }
     
     func rightButtonTouchUp(button: UIButton) {
@@ -430,6 +471,35 @@ extension SettingViewController: UITableViewDataSource {
             self.attachResetButton(superView: cell)
             self.resetButton.isHidden = !self.isLocked
         }
+        
+        if self.settingViewUsage == .info && cellInfo.labelText.isEmpty {
+            self.attachWithdrawalButton(superView: cell)
+            let separatorView = cell.subviews.first?.subviews.filter {$0.restorationIdentifier == "SeparatorView"}.first
+            separatorView?.isHidden = true
+        }
         return cell
+    }
+}
+
+// MARK: - API Services
+
+extension SettingViewController {
+    private func deleteUserWithAPI(userId: Int, completion: @escaping () -> Void) {
+        UserService.shared.deleteUser(userId: userId) { result in
+            switch result {
+            case .success:
+                DispatchQueue.main.async {
+                    completion()
+                }
+            case .requestErr(let errorMessage):
+                print(errorMessage)
+            case .pathErr:
+                print("pathErr")
+            case .serverErr:
+                print("serverErr")
+            case .networkFail:
+                print("networkFail")
+            }
+        }
     }
 }
