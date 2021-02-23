ec640cf44515690b525a9de1cf68179ccd589d1a
jeongyeob97 <leejeongyeob97@gmail.com>
2021-02-24 00:59:51 +0900

    [#361] 통계뷰 일기 숫자 라벨 업데이트 하는 함수 추가

--- MoMo/MoMo/Sources/ViewControllers/StatisticsViewController.swift
+++ MoMo/MoMo/Sources/ViewControllers/StatisticsViewController.swift
@@ -26,6 +26,7 @@ class StatisticsViewController: UIViewController {
     @IBOutlet weak var moodButton: UIButton!
     @IBOutlet weak var statContainerView: UIView!
     @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var statNumLabel: UILabel!
     
     // MARK: - Override LifeCycle
     override func viewDidLoad() {
@@ -213,6 +214,10 @@ class StatisticsViewController: UIViewController {
         }
     }
     
+    private func updateStatNumLabel(_ cnt: Int) {
+        self.statNumLabel.text = "\(cnt)개의 일기"
+    }
+    
     private func updateMoodDepthData(_ emotion: [EmotionCount], _ depth: [DepthCount]){
         depthData = [0, 0, 0, 0, 0, 0, 0]
         emotionData = [0, 0, 0, 0, 0, 0, 0, 0]
@@ -223,6 +228,8 @@ class StatisticsViewController: UIViewController {
         for idx in 0..<depth.count {
             depthData[depth[idx].depth] = depth[idx].count
         }
+        updateStatNumLabel(depthData.reduce(0, +))
+        
     }
     
     // MARK: - Selector Function
