aaab56989ea1e9e68f2dc557880a022a0b70b444
iamcho2 <thatgrass@naver.com>
2021-03-03 00:36:11 +0900

    [#406], [#409] edge pan gesture 로직 수정

--- MoMo/MoMo/Sources/Extensions/UINavigationControllerDelegate+Extension.swift
+++ MoMo/MoMo/Sources/Extensions/UINavigationControllerDelegate+Extension.swift
@@ -19,80 +19,23 @@ extension UINavigationController: UINavigationControllerDelegate {
         
         if responds(to: #selector(getter: self.interactivePopGestureRecognizer)) {
             
-            // 온보딩부터 시작하는지 검사
-            if viewControllers[0].isKind(of: OnboardingViewController.self) {
-                
-                if viewControllers.count > 1 {
-                    
-                    // 분기 1 : 온보딩으로 진입했을 때
-                    if viewControllers[1].isKind(of: MoodViewController.self) {
-                        
-                        // 분기 2 : 온보딩이 끝났을 때
-                        if viewControllers.count > 6 {
-                            interactivePopGestureRecognizer?.isEnabled = true
-                            
-                            // 분기 3 : 이메일 로그인을 한 후 홈으로 갔을 때 홈에서 로그인창으로 가는 pan gesture 비활성화
-                            if viewControllers[6].isKind(of: EmailLoginViewController.self) {
-                                
-                                if viewControllers.count == 8 {
-                                    interactivePopGestureRecognizer?.isEnabled = false
-                                } else {
-                                    interactivePopGestureRecognizer?.isEnabled = true
-                                }
-                            }
-                            
-                            if viewControllers.count > 7 {
-                                // 분기 4 : 회원가입을 한 후 홈으로 갔을 때 홈에서 회원가입창으로 가는 pan gesture 비활성화
-                                if viewControllers[7].isKind(of: JoinViewController.self) {
-                                    
-                                    if viewControllers.count == 9 {
-                                        interactivePopGestureRecognizer?.isEnabled = false
-                                    } else {
-                                        interactivePopGestureRecognizer?.isEnabled = true
-                                    }
-                                }
-                            }
-                        
-                        // 분기 2 : 온보딩이 진행중일 때
-                        } else {
-                            interactivePopGestureRecognizer?.isEnabled = false
-                        }
-                    
-                    // 분기 1 : '이미 계정이 있어요' 버튼을 눌렀을 때
-                    } else if viewControllers[1].isKind(of: LoginViewController.self) {
-                        
-                        if viewControllers.count > 2 {
-                            interactivePopGestureRecognizer?.isEnabled = true
-                            
-                            // 분기 3 : 이메일 로그인을 한 후 홈으로 갔을 때 홈에서 로그인창으로 가는 pan gesture 비활성화
-                            if viewControllers[2].isKind(of: EmailLoginViewController.self) {
-                                
-                                if viewControllers.count == 4 {
-                                    interactivePopGestureRecognizer?.isEnabled = false
-                                } else {
-                                    interactivePopGestureRecognizer?.isEnabled = true
-                                }
-                            }
-                            
-                            if viewControllers.count > 3 {
-                                // 분기 4 : 회원가입을 한 후 홈으로 갔을 때 홈에서 회원가입창으로 가는 pan gesture 비활성화
-                                if viewControllers[3].isKind(of: JoinViewController.self) {
-                                    
-                                    if viewControllers.count == 5 {
-                                        interactivePopGestureRecognizer?.isEnabled = false
-                                    } else {
-                                        interactivePopGestureRecognizer?.isEnabled = true
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
+            // 현재 VC가 Home, Login, teamMOMO일 때 gesture 비활성화
+            if viewController.isKind(of: HomeViewController.self) || viewController.isKind(of: LoginViewController.self) || viewController.isKind(of: TeamViewController.self) {
+                interactivePopGestureRecognizer?.isEnabled = false
             } else {
-                if viewControllers.count > 1 {
-                    interactivePopGestureRecognizer?.isEnabled = true
-                }
+                interactivePopGestureRecognizer?.isEnabled = true
             }
+            
+            // 다이어리 작성 후 diary VC 진입 시 gesture 비활성화
+            if viewControllers.count > 1 {
+                
+                if viewController.isKind(of: DiaryViewController.self) && viewControllers[viewControllers.count - 2].isKind(of: DeepViewController.self) {
+                    interactivePopGestureRecognizer?.isEnabled = false
+                } 
+            } else {
+                interactivePopGestureRecognizer?.isEnabled = true
+            }
+            
         }
     }
 }
