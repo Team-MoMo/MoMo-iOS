7494e5698b2c774e0f61042f94d498aa4f18031f
jeongyeob97 <leejeongyeob97@gmail.com>
2021-03-03 23:20:41 +0900

    [#420] 팀모모뷰에서 다른 뷰로 전환할 때   네비색 변하는 거  수정

--- MoMo/MoMo/Sources/ViewControllers/TeamViewController.swift
+++ MoMo/MoMo/Sources/ViewControllers/TeamViewController.swift
@@ -16,14 +16,11 @@ class TeamViewController: UIViewController {
         return image
     }()
     
-    let mainColor: UIColor? =  UIColor(red: 238/255, green: 249/255, blue: 255/255, alpha: 1.0)
     // MARK: - Override ViewLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         initializeNavigationBarColor()
         intializeNavigationItem()
-        initializeStatusBarColor()
-        
     }
     
     // MARK: - Private Functions
@@ -35,36 +32,9 @@ class TeamViewController: UIViewController {
     }
     
     private func initializeNavigationBarColor() {
-        guard let color = mainColor else {
-            return
-        }
-        self.navigationController?.navigationBar.backgroundColor = color
-    }
-    
-    private func initializeStatusBarColor() {
-        guard let color = mainColor else {
-            return
-        }
-        if #available(iOS 13.0, *) {
-            let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
-            guard let statusBarHeight = window?.windowScene?.statusBarManager?.statusBarFrame.height else {
-                return
-            }
-            
-            let statusbarView = UIView()
-            statusbarView.backgroundColor = color
-            view.addSubview(statusbarView)
-          
-            statusbarView.translatesAutoresizingMaskIntoConstraints = false
-            statusbarView.heightAnchor.constraint(equalToConstant: statusBarHeight).isActive = true
-
-            statusbarView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1.0).isActive = true
-            statusbarView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-            statusbarView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        } else {
-            let statusBar = UIApplication.shared.value(forKeyPath: "statusBarWindow.statusBar") as? UIView
-            statusBar?.backgroundColor = color
-        }
+        self.navigationController!.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController!.navigationBar.shadowImage = UIImage()
+        self.navigationController!.navigationBar.isTranslucent = true
     }
     
     // MARK: - objc Function
